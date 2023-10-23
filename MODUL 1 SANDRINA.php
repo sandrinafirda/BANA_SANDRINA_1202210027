556ecd7e289dc762ca6d3826e54fd802222a4d94
sandrinafirda <147320254+sandrinafirda@users.noreply.github.com>
2023-10-23 12:27:58 +0700

    Add files via upload

---
+++ MODUL 1 SANDRINA.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Portofolio Sandrina </title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+header {
+  background-color: #8EACCD;
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+  color: rgb(0, 0, 0);
+}
+
+/* Create two columns/boxes that floats next to each other */
+nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  float: left;
+  width: 30%;
+  height: 600px; /* only for demonstration, should be removed */
+  background: #F9F3CC;
+  padding: 20px;
+
+}
+
+/* Style the list inside the menu */
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+
+  float: left;
+  padding: 30px;
+  width: 70%;
+  background-color: #fafafa;
+  height: 300px; /* only for demonstration, should be removed */
+}
+    h1 {
+        color: rgb(0, 0, 0);
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-weight: 800;
+    }
+    h2 {
+        color: rgb(0, 0, 0);
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+       
+    }
+    h3 {
+        color: rgb(25, 25, 94);
+        font-size: 16px;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+    p1 {
+        color: rgb(0, 0, 0);
+        font-size: 20px;
+        font-style: italic;
+    }
+    p2 {
+        color: rgb(0, 0, 0);
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-weight: 900;
+        align-content: center;
+    }
+</style>
+<header>
+<p2>PORTOFOLIO</p2>
+</header> 
+
+<nav>
+    <img src= SANDRINA.jpg width="150 px">
+    <div class="card mt-5" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Business Card</h5>
+          <p class="card-text">Find out more!</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+</nav>
+<tr></tr>
+
+
+<article>
+<h1 >Sandrina Firda Arfiananda</h1>
+<p1> You can call me Sandrina </p1>
+<p>I'm a growth marketer, digital strategist, entrepreneur, and brand builder</p>
+<p>and brand builder who love freash ideas, ambitious challenges,</p>
+<p>fast-paced startups, and energizing conversations</p>
+
+
+    
+</article>
+
+
+
+</body>
+</html>
